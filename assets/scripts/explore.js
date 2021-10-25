52428b1b9d77950fb42a5281a1255981929d5af3
Tarek Tarif <ttarif@ucsd.edu>
2021-10-24 22:16:52 -0700

    explore completed

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -15,7 +15,6 @@ function populateVoices() {
     for(var i = 0; i < dialects.length ; i++) {
       var option = document.createElement('option');
       option.textContent = dialects[i].name + ' (' + dialects[i].lang + ')';
-  
       if(dialects[i].default) {
         option.textContent += ' -- DEFAULT';
       }
@@ -25,17 +24,17 @@ function populateVoices() {
       dialect_selection.appendChild(option);
     }
   };
-
   populateVoices();
+
   if (speechSynthesis.onvoiceschanged !== undefined) {
     speechSynthesis.onvoiceschanged = populateVoices;
   };
   
   play_button.addEventListener('click', (event) => {
     var speech = new SpeechSynthesisUtterance(text.value);
-    var selectedOption = dialect_selection.selectedOptions[0].getAttribute('data-name');
+    var selected_option = dialect_selection.selectedOptions[0].getAttribute('data-name');
     for (var i = 0; i < dialects.length; i++) {
-      if(dialects[i].name === selectedOption) {
+      if(dialects[i].name === selected_option) {
         speech.voice = dialects[i];
       }
     }
@@ -43,12 +42,10 @@ function populateVoices() {
     speech.addEventListener('start', (event) =>{
       faceimage.src = 'assets/images/smiling-open.png';
     });
-
     speech.addEventListener('end', (event) => {
       faceimage.src = 'assets/images/smiling.png';
     });
-    
+
     synthesis.speak(speech);
-    text.blur();
   });
 }
